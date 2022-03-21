3b6838ab88c084d4e44a4898c3e2e037a77399e2
hmdnz <hammadmcferri@gmail.com>
2022-03-21 10:30:10 +0000

    save

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className='App'>
+//       <header className='App-header'>
+//         <img src={logo} className='App-logo' alt='logo' />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <h1 className='h1'>Learn React</h1>
+//         <a
+//           className='App-link'
+//           href='https://reactjs.org'
+//           target='_blank'
+//           rel='noopener noreferrer'
+//         ></a>
+//       </header>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
+
+class App extends Component{
+  constructor() {
+    super()
+
+    this.state = {
+     monsters:[
+       {
+         name: 'Frankenstein', 
+         id: 'asr12'
+        },
+        {
+          name: 'Dracula',
+          id: 'asc212'
+        },
+        {
+          name: 'Zombie',
+          id: 'adc45'
+        }
+    ]
+    }
+  }
+  render(){
+    return (
+      <div className = 'App'>
+        {
+        this.state.monsters.map(monster => (  
+        <h1 key={monster.id}>{monster.name}</h1>
+
+        ))}
+      </div>
+    
+    );
+
+
+export default App
